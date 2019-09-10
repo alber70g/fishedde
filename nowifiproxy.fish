98d04c836c3f25c96c65a057d9d79a76c49821bd
Albert Groothedde <albertgroothedde@gmail.com>
2019-09-10 18:31:02 +0200

    Add wifiproxy toggles

---
+++ nowifiproxy.fish
@@ -0,0 +1,5 @@
+function nowifiproxy 
+#  networksetup -setautoproxystate Wi-Fi off
+  networksetup -setwebproxystate Wi-Fi off
+  networksetup -setsecurewebproxystate Wi-Fi off 
+end
---
+++ setwifiproxy.fish
@@ -0,0 +1,5 @@
+function setwifiproxy 
+#  networksetup -setautoproxystate Wi-Fi on
+  networksetup -setwebproxystate Wi-Fi on
+  networksetup -setsecurewebproxystate Wi-Fi on 
+end
