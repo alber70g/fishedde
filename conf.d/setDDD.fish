b4163c45964e82cacd24e23c5e19078c639bf40a
Albert Groothedde <albertgroothedde@gmail.com>
2019-09-11 17:22:30 +0200

    Resource ... and .... with setDDD and setDDDD

--- conf.d/setDDD.fish
+++ conf.d/setDDD.fish
@@ -1,6 +1,9 @@
 # If you have fixes, or ways to make the commands better, please
 # [fork](https://github.com/alber70g/fishedde/fork) and send a pull request
 
-function ... -d "go up two levels"
-  cd ../../
+function setDDD
+  function ... -d "go up two levels"
+    cd ../../
+  end
 end
+setDDD
--- conf.d/setDDDDD.fish
+++ conf.d/setDDDDD.fish
@@ -1,6 +1,9 @@
 # If you have fixes, or ways to make the commands better, please
 # [fork](https://github.com/alber70g/fishedde/fork) and send a pull request
 
-function .... -d "go up three levels"
-  cd ../../../
+function setDDDD
+  function .... -d "go up three levels"
+    cd ../../../
+  end
 end
+setDDDD
